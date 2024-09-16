02f1ce16a2d3a93c67111c78fc5675e9d3c4f0c7
james.hsueh <CodeMachine0121@gmail.com>
2024-09-16 18:56:49 +0800

    feat: implement get create and update

--- RedisSdkV2/RedisService.cs
+++ RedisSdkV2/RedisService.cs
@@ -1,10 +1,38 @@
-﻿namespace RedisSdkV2;
+﻿using System.Text.Json;
+using StackExchange.Redis;
+
+namespace RedisSdkV2;
 
 public interface IRedisService
 {
 }
 
-public class RedisService : IRedisService
+public class RedisService(IConnectionMultiplexer connectionMultiplexer) : IRedisService
 {
-    
+    private readonly IDatabase _redis = connectionMultiplexer.GetDatabase();
+
+    public T GetOrCreate<T>(string key, Func<T> func)
+    {
+        var redisValue = _redis.StringGet(key);
+        if (redisValue.IsNullOrEmpty)
+        {
+            _redis.StringSet(key, func.Invoke()!.ToString(), TimeSpan.FromSeconds(10));
+        }
+        
+        return  JsonSerializer.Deserialize<T>(_redis.StringGet(key).ToString())!;
+    }
+
+    public void Update<T>(string key, T value)
+    {
+        var redisValue = _redis.StringGet(key);
+        if (redisValue.IsNullOrEmpty)
+        {
+            throw new KeyNotFoundException("Key not found in Redis");
+        }
+        
+        _redis.StringSet(key, JsonSerializer.Serialize(value), TimeSpan.FromSeconds(10));
+    }
+
+
+
 }
