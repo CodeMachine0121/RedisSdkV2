cd15c84a3f3124d752e3a7163ef689a78271bb10
james.hsueh <CodeMachine0121@gmail.com>
2024-09-17 10:40:31 +0800

    feat: implement redis lock

--- RedisSdkV2/InjectionConfig.cs
+++ RedisSdkV2/InjectionConfig.cs
@@ -5,18 +5,33 @@ namespace RedisSdkV2;
 
 public static class InjectionConfig
 {
-
-    public static void UseRedisSdkForSingleNode(this IServiceCollection services, string redisHostAddress)
+    public static void UseRedisSdkForSingleNode(this IServiceCollection services, string redisHostAddress,
+        bool isRedLock = false)
     {
         services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisHostAddress));
-        services.AddTransient<IRedisService, RedisService>();
+
+        if (isRedLock)
+        {
+            services.AddTransient<IRedisService, RedisRedLockService>();
+        }
+        else
+        {
+            services.AddTransient<IRedisService, RedisService>();
+        }
     }
 
-    public static void UseRedisSdkForMultipleNodes(this IServiceCollection services, List<string> redisHostAddressList)
+    public static void UseRedisSdkForMultipleNodes(this IServiceCollection services, List<string> redisHostAddressList, bool isRedLock = false)
     {
-        var connectionMultiplexers = redisHostAddressList.Select(x=> ConnectionMultiplexer.Connect(x)).ToList();
-        connectionMultiplexers.ForEach(x=> services.AddSingleton<IConnectionMultiplexer>(x));
+        var connectionMultiplexers = redisHostAddressList.Select(x => ConnectionMultiplexer.Connect(x)).ToList();
+        if (isRedLock)
+        {
+            connectionMultiplexers.ForEach(x=> 
+                services.AddSingleton<IConnectionMultiplexer>(x));
+        }
+        else
+        {
+            connectionMultiplexers.ForEach(x => 
+                services.AddSingleton<IConnectionMultiplexer>(x));
+        }
     }
-    
-    
 }
--- RedisSdkV2/RedisRedLockService.cs
+++ RedisSdkV2/RedisRedLockService.cs
@@ -1,13 +1,12 @@
-using System.Text.Json;
 using RedLockNet.SERedis;
 using RedLockNet.SERedis.Configuration;
 using StackExchange.Redis;
 
 namespace RedisSdkV2;
 
-public class RedisRedLockService(IConnectionMultiplexer connectionMultiplexer, IRedisService redisService) : IRedisRedLockService
+public class RedisRedLockService(IConnectionMultiplexer connectionMultiplexer, IRedisService redisService)
+    : IRedisService
 {
-    private readonly IDatabase _database = connectionMultiplexer.GetDatabase();
     private readonly RedLockFactory _redLockFactory = RedLockFactory.Create(new List<RedLockMultiplexer>
     {
         new(connectionMultiplexer)
@@ -16,31 +15,28 @@ public class RedisRedLockService(IConnectionMultiplexer connectionMultiplexer, I
 
     public async Task<T> GetOrCreate<T>(string key, Func<T> func, TimeSpan timeout)
     {
-        var redisValue = await _database.StringGetAsync(key);
-        if (!redisValue.IsNullOrEmpty)
-        {
-            return JsonSerializer.Deserialize<T>(redisValue)!;
-        }
-
-        var redLock = await _redLockFactory.CreateLockAsync($"lock-{key}", timeout, TimeSpan.FromSeconds(10),
+        await using var redLock = await _redLockFactory.CreateLockAsync($"lock-{key}", timeout, TimeSpan.FromSeconds(10),
             TimeSpan.FromSeconds(5));
-
+        
         if (redLock.IsAcquired)
         {
-           return await redisService.GetOrCreate<T>(key, func, timeout);
+            return await redisService.GetOrCreate(key, func, timeout);
         }
 
         throw new RedisException("Could not acquire a lock");
     }
 
-    public void Update<T>(string key, T value)
+    public async Task Update<T>(string key, T value)
     {
-        throw new NotImplementedException();
+        await using var redLock = await _redLockFactory.CreateLockAsync($"lock-{key}", TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(10),
+            TimeSpan.FromSeconds(5));
+        
+        if (redLock.IsAcquired)
+        {
+            await redisService.Update(key, value);
+            
+        }
+        
+        throw new RedisException("Could not acquire a lock");
     }
-}
-
-public interface IRedisRedLockService
-{
-    void Update<T>(string key, T value);
-    Task<T> GetOrCreate<T>(string key, Func<T> func, TimeSpan timeout);
 }
